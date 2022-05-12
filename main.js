89840a4fbde3f1522114e8a5b9785b0c5a456c7b
Chainamnaris, Sutthidol <87947943+ChainamnarisSutthidol@users.noreply.github.com>
2022-05-11 17:35:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song=""
+song1="";
+
+function preload(){
+    song=loadSound("rickroll.mp3");
+    song1=loadSound("sovietmarch.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
